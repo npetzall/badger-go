a88f07e8722bae944afa895f476516fd910e42e0
Nils Petzaell <nils.petzall@gmail.com>
2016-10-28 08:51:35 +0200

    Added logging framework

--- badger.go
+++ badger.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/npetzall/badger-go/lib/badge"
 	"github.com/npetzall/badger-go/lib/nexus"
+	logging "github.com/op/go-logging"
 )
+
+var log = logging.MustGetLogger("badger-go")
+
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+func GetLogger() *logging.Logger {
+	return log
+}
 
 func createBadge(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	err, data := badge.CreateBadge(q.Get("l"), q.Get("r"), q.Get("c"))
 	if err != nil {
+		log.Error(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	} else {
 		w.Header().Add("Content-Type", "image/svg+xml")
 		w.Write(data)
 	}
+}
+
+func init() {
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, format)
+	logging.SetBackend(backendFormatter)
 }
 
 func main() {
@@ -32,5 +51,6 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+	log.Infof("Starting server at: %s", srv.Addr)
 	log.Fatal(srv.ListenAndServe())
 }
